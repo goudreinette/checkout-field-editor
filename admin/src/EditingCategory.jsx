7736c33ae223aa822b7689bf693b26a1e47c1903
Rein van der Woerd <rwoerd2033@student.landstede.nl>
2016-10-13 12:20:48 +0200

    nope

--- admin/src/EditingCategory.jsx
+++ admin/src/EditingCategory.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
-import autosize from 'autosize-input'
 
-export default function EditingCategory (props)
+export default function EditingCategory(props)
 {
   return (
     <input
       className='category active'
       value={props.name}
       onChange={e => props.updateName(e.target.value)}
-      onKeyDown={e => e.keyCode === 13 ? props.stopEditing() : null }
+      onKeyDown={e => e.keyCode === 13 ? props.stopEditing() : null}
       autoFocus={true}
-    />
+      />
   )
 }
