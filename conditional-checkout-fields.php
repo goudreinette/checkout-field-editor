54abc315e85f1bf497c851c23ffd8fb546ff3a29
Rein van der Woerd <rwoerd2033@student.landstede.nl>
2016-10-15 16:31:48 +0200

    remaining feature files

--- conditional-checkout-fields.php
+++ conditional-checkout-fields.php
@@ -6,9 +6,11 @@ Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
 Description: A brief description of the Plugin.
 Version: 1.0
 Author: reinvdwoerd
-Author URI: http://URI_Of_The_Plugin_Author
+Author URI: reinvdwoerd.herokuapp.com
 License: A "Slug" license name e.g. GPL2
 */
 
 require plugin_dir_path(__FILE__) . "./admin.php";
 require plugin_dir_path(__FILE__) . "./option.php";
+require plugin_dir_path(__FILE__) . "./checkout.php";
+require plugin_dir_path(__FILE__) . "./emails.php";
