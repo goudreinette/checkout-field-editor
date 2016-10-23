ce76d3eff5662eb1ab4027ba584b16752992df8d
reinvdwoerd <reinvanderwoerd@me.com>
2016-10-23 21:17:42 +0200

    PHP 5.4 fix

--- build.exs
+++ build.exs
@@ -8,7 +8,7 @@ File.mkdir_p!("dist/admin")
 
 File.cp!("checkout-field-editor.php", "dist/checkout-field-editor.php")
 File.cp_r!("source", "dist/source")
-File.cp_r!("vendor", "dist/vendor")
+File.cp_r!("vendor/composer", "dist/vendor/composer/")
 File.cp_r!("admin/build", "dist/admin/build")
 
 :zip.create('dist.zip', ['dist'])
